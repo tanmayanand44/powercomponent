f062555fc0982a1bb4be88bc0e11f7a2694e6c6c
tanmayanand44 <anand44tanmay@gmail.com>
2018-05-13 16:08:11 +0530

    power component

---
+++ heroes.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'power',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+}
