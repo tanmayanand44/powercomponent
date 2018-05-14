51fb4a8f96f119fa00482ff923141de4c289357a
Tanmay Anand <33908900+tanmayanand44@users.noreply.github.com>
2018-05-14 15:55:57 +0530

    Update heroes.component.ts

--- heroes.component.ts
+++ heroes.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-
+@Pipe({name: 'raise'})
+export class ExponentialStrengthPipe implements PipeTransform {
+  transform(value: number, exponent: string): number {
+    let exp = parseFloat(exponent);
+    return Math.pow(value, isNaN(exp) ? 1 : exp);
+  }
+}
 @Component({
   selector: 'power',
   templateUrl: './heroes.component.html',
